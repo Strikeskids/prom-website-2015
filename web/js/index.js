497ace1abca0622769185435c5a3e9e449f2e400
Strikeskids <github@strikeskids.com>
2015-04-27 11:32:30 -0400

    Make multiple buttons

--- web/js/index.js
+++ web/js/index.js
@@ -2,26 +2,32 @@ jQuery(function($) {
     var form = $('#login-form')
     var formFields = form.find('input, select, textarea')
     var formSubmit = form.find('button[type="submit"]')
-    
+    var formRegister = null
+
     form.on('submit', function(e) {
         e.preventDefault()
 
-        if (formSubmit.hasClass('create')) {
+        var $targ = $(e.target)
+
+        if ($targ.hasClass('create')) {
             api.query('GET', '/api/register', formFields.serialize())
                 .done(function(json) {
-                    formSubmit.apiNotify(json, '/question1')
+                    $targ.apiNotify(json, '/question1')
                 })
         } else {
             api.query('POST', '/api/login', formFields.serialize())
                 .done(function(json) {
                     if (!json.status) {
-                        formSubmit.apiNotify(json)
-                        if (json.data && json.data.button) {
-                            formSubmit.text(json.data.button)
-                            formSubmit.addClass('create')
+                        $targ.apiNotify(json)
+                        if (json.data && json.data.button && !formRegister) {
+                            formRegister = $('<button>')
+                                .prop('type', 'submit')
+                                .addClass('create')
+                                .text('Join')
+                                .insertAfter(formSubmit)
                         }
                     } else {
-                        formSubmit.apiNotify(json, json.data.next)
+                        $targ.apiNotify(json, json.data.next)
                     }
                 })
         }
