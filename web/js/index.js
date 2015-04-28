591d2bdab05922d9057b4718fdc87c830b44268a
Strikeskids <github@strikeskids.com>
2015-04-27 22:03:28 -0400

    Always have two action buttons on index

--- web/js/index.js
+++ web/js/index.js
@@ -2,36 +2,23 @@ jQuery(function($) {
     var form = $('#login-form')
     var formFields = form.find('input, select, textarea')
     var formSubmit = form.find('button[type="submit"]')
-    var formRegister = null
+    var formRegister = form.find('button[type="button"]')
     
-    var register = function(e) {
+    formRegister.on('click', function(e) {
         e.preventDefault()
 
         api.query('POST', '/api/register', formFields.serialize())
             .done(function(json) {
                 formRegister.apiNotify(json, '/question1')
             })
-    }
-    
+    })
 
     form.on('submit', function(e) {
         e.preventDefault()
 
         api.query('POST', '/api/login', formFields.serialize())
             .done(function(json) {
-                if (!json.status) {
-                    formSubmit.apiNotify(json)
-                    if (json.data && json.data.button && !formRegister) {
-                        formRegister = $('<button>')
-                            .prop('type', 'button')
-                            .text('Join')
-                            .on('click', register)
-                            .css('margin-left', '20px')
-                            .insertAfter(formSubmit)
-                    }
-                } else {
-                    formSubmit.apiNotify(json, json.data.next)
-                }
+                formSubmit.apiNotify(json, json.data && json.data.next)
             })
     })
 })
